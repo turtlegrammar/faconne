9082ddd6be18be53420b9d2cfc48a4922cc67195
j <disalvjn@mail.uc.edu>
2016-02-02 20:26:17 -0500

    Rewrote using doseq & transients and more sensible destructuring.
    Should be able to add filtering and extracting easily.

--- src/plum/core.clj
+++ src/plum/core.clj
@@ -1,84 +1,147 @@
 (ns plum.core
   (:require [clojure.set :as set]))
 
-(defn join
-  "For coll a: [a] -> a"
-  [& things]
-  (let [non-nil (filter identity things)
-        ;; this feels horrible
-        thing   (first non-nil)]
-    (cond (map? thing)
-          (apply merge-with join non-nil)
+(defn create-assoc!
+  [m k v]
+  (assoc! (or m (transient {})) k v))
 
-          (or (list? thing) (vector? thing))
-          (reduce into non-nil)
+(defn assoc-in!
+  [m [k & ks] v]
+  (if-not ks
+    (create-assoc! m k v)
+    (create-assoc! m k (assoc-in! (get m k) ks v))))
 
-          (set? thing)
-          (reduce set/union non-nil))))
+(defn update!
+  [m k f]
+  (create-assoc! m k (f (get m k))))
 
-(defn- map-kv-binding
-  [m]
-  (first (filter (fn [[k _]] (symbol? k)) m)))
+(defn update-in!
+  [m [k & ks] f]
+  (if-not ks
+    (update! m k f)
+    (create-assoc! m k (update-in! (get m k) ks f))))
 
-(declare make-collector)
+(defn deep-persistent!
+  [nest coll]
+  (if (= nest 0)
+    (persistent! coll)
+    (->> (persistent! coll)
+         (map (fn [[k v]] [k (deep-persistent! (dec nest) v)]))
+         (into {}))))
 
-(defn- map-collector
-  [from to]
-  (let [[keysym valstructure] (map-kv-binding from)
-        mapsym (gensym "map")
-        extractor (or (:extract from) `identity)
-        where (or (:where from) `true)
-        bindings (:let from)
-        bound-entries (vec (vals (dissoc bindings :as :rest)))
-        binding-clauses (reduce (fn [acc [k v]]
-                                  (if (symbol? k)
-                                    (into acc [k `(get ~mapsym ~v)])
-                                    (case k
-                                      :as   (into acc [v mapsym])
-                                      :rest (into acc [v `(apply dissoc ~mapsym ~bound-entries)])
-                                      (throw (Exception. (str "Unsupported binding " k))))))
-                                []
-                                bindings)]
-    `(fn [~mapsym]
-       (let ~binding-clauses
-         (if ~where
-           ~(if keysym
-              `(apply join
-                      (map (fn [[~keysym rest#]]
-                             (~(make-collector valstructure to) rest#))
-                           (~extractor ~mapsym)))
-              to))))))
+(defn why-i-disallow-certain-destructuring
+  [form]
+  (str "Form " form " is disallowed because the form {k v} is ambiguous when k is a "
+       "symbol and v is a symbol or collection. For example {k v} is either "
+       "destructuring a map, binding `k` to (get m 'v), or it's transform-specific "
+       "(doseq through the map, binding each tuple to [k v], building a result). "
+       "Because of this ambiguity in certain cases, I've disallowed destructuring maps "
+       "with the form {k :key} when the destructuring map has only one entry. If you "
+       "want to use this style of destructuring (instead of :keys etc.), then "
+       "you need at lest two entries in the map to disambiguate it."))
 
-(defn- vec-set-collector
-  [from to]
-  `(fn [coll#]
-     (apply join (map (fn [x#] (~(make-collector (first from) to) x#))
-                      coll#))))
+(defn parse-domain
+  [x]
+  (cond (symbol? x)
+        {:type :leaf, :bind x}
 
-(defn- make-collector
-  [from to]
-  (cond (map? from)
-        (map-collector from to)
+        (vector? x)
+        (if (> (count x) 1)
+          ;; x is a vector destructuring form
+          {:type :leaf, :bind x}
 
-        (or (vector? from) (set? from))
-        (vec-set-collector from to)
+          ;; x = [s] -- s is non-terminal
+          {:type :seq, :child (parse-domain (first x))})
 
-        :else
-        `(fn [~from] ~to)))
+        (set? x)
+        ;; x = #{s} -- s is non-terminal
+        {:type :seq, :child (parse-domain (first x))}
 
-(defmacro collector
-  [from to]
-  (make-collector from to))
+        (map? x)
+        (if (or ((some-fn :strs :keys :syms) x)
+                (> (count x) 1))
+          {:type :leaf, :bind x}
+          (let [[k v] (first x)]
+            (if-not (or (coll? v) (symbol? v))
+              (throw (Exception. (why-i-disallow-certain-destructuring {k v})))
+              {:type :map, :bind k, :child (parse-domain v)})))))
 
-(defmacro collect
-  [from to coll]
-  (let [f (make-collector from to)]
-    `(~f ~coll)))
+(defn parse-range
+  [structure]
+  (let [empty-result (cond (map? structure) {}
+                           (set? structure) #{}
+                           (vector? structure) [])]
+    (loop [structure structure
+           path []
+           type nil
+           empty nil
+           nest 0]
+      (cond (not (coll? structure))
+            {:path path :type type
+             :leaf structure :empty empty-result
+             :nest (dec nest) :empty-leaf-cont empty}
 
-(defn key=
-  [& keys]
-  #(select-keys % keys))
+            (map? structure)
+            (let [[k v] (first structure)]
+              (recur v (conj path k) :map {} (inc nest)))
 
-(defn key!=
-  [& keys]
-  #(apply dissoc % keys))
+            (or (vector? structure) (set? structure))
+            {:path path
+             :leaf (first structure)
+             :type (if (= type :map)
+                     :seq-in-map
+                     :seq)
+             :empty-leaf-cont (if (vector? structure) [] #{})
+             :empty empty-result
+             :nest nest}))))
+
+(defn modifier-clause
+  [{:keys [path leaf type empty empty-leaf-cont]}]
+  (fn [result-sym]
+    (case type
+      :seq `((fnil conj! (transient ~empty)) ~result-sym ~leaf)
+
+      :map `(assoc-in! ~result-sym ~path ~leaf)
+
+      :seq-in-map `(update-in! ~result-sym ~path
+                               (fn [x#] ((fnil conj! (transient ~empty-leaf-cont))
+                                         x# ~leaf))))))
+
+(defn compile
+  [domain range]
+  (let [result-sym    (gensym "result")
+        structure-sym (gensym "structure")
+        parsed-domain (parse-domain domain)
+        parsed-range  (parse-range range)
+        modifier      ((modifier-clause parsed-range) result-sym)
+        default-value (:empty parsed-range)
+        nest          (-> parsed-range :nest)]
+    (letfn
+        [(go [{:keys [type bind child leaf]} ;; parsed domain
+              struct-sym]
+             (let [next-sym (gensym "val")]
+               (case type
+                 :map
+                 `(doseq [[~bind ~next-sym] ~struct-sym]
+                    ~(go child next-sym))
+
+                 :seq
+                 `(doseq [~next-sym ~struct-sym]
+                    ~(go child next-sym))
+
+                 :leaf
+                 `(let [~bind ~struct-sym]
+                    ~modifier))))]
+      `(fn [~structure-sym]
+         (let [~result-sym (transient ~default-value)]
+           ~(go parsed-domain structure-sym)
+           (deep-persistent! ~nest ~result-sym))))))
+
+(defmacro transformer
+  [domain range]
+  (compile domain range))
+
+(defmacro transform
+  [domain range x]
+  `(let [f# (transformer ~domain ~range)]
+     (f# ~x)))
--- test/plum/test/core.clj
+++ test/plum/test/core.clj
@@ -2,137 +2,150 @@
   (:require [plum.core :as p])
   (:use [clojure.test]))
 
-(deftest simple-collections
-  (let [m {"Sussman" {"AI"
-                      [{:name "John"
-                        :grade "A"}
-                       {:name "Sally"
-                        :grade "B"}]
-                      "Compilers"
-                      [{:name "Tom"
-                        :grade "B"}
-                       {:name "John"
-                        :grade "B"}]}
-           "Abelson" {"Machine Learning"
-                      [{:name "Sally"
-                        :grade "C"}
-                       {:name "Tom"
-                        :grade "B-"}]
-                      "Compilers"
-                      [{:name "Eva Lu Ator"
-                        :grade "B"}
-                       {:name "Ben Bitdiddle"
-                        :grade "A"}]}}
-        profs-per-student {"John" #{"Sussman"} "Sally" #{"Abelson" "Sussman"}
-                           "Tom" #{"Abelson" "Sussman"} "Eva Lu Ator" #{"Abelson"}
-                           "Ben Bitdiddle" #{"Abelson"}}]
+(def profs->classes->students
+  {"Sussman" {"AI"
+              [{:name "John"
+                :grade "A"}
+               {:name "Sally"
+                :grade "B"}]
+              "Compilers"
+              [{:name "Tom"
+                :grade "B"}
+               {:name "John"
+                :grade "B"}]}
+   "Abelson" {"Machine Learning"
+              [{:name "Sally"
+                :grade "C"}
+               {:name "Tom"
+                :grade "B-"}]
+              "Compilers"
+              [{:name "Eva Lu Ator"
+                :grade "B"}
+               {:name "Ben Bitdiddle"
+                :grade "A"}]}})
 
-    (is (= (p/collect {prof {_ [student]}}
+(def students->profs {"John" #{"Sussman"} "Sally" #{"Abelson" "Sussman"}
+                      "Tom" #{"Abelson" "Sussman"} "Eva Lu Ator" #{"Abelson"}
+                      "Ben Bitdiddle" #{"Abelson"}})
+
+(deftest map->map
+  (let [m {:a {:b 2 :c 5} :c {:b 3 :e 1}}]
+    (is (= (p/transform {k1 {k2 v}}
+                        {k2 {k1 v}}
+                        m)
+           {:b {:a 2
+                :c 3}
+            :c {:a 5}
+            :e {:c 1}}))))
+
+(deftest set-in-map
+  (is (= (p/transform {prof {_ [student]}}
                       {(:name student) #{prof}}
-                      m)
-           profs-per-student))
+                      profs->classes->students)
+         students->profs))
 
-    (is (= (p/collect {prof {_ [{:let {student :name}}]}}
-                      {student #{prof}}
-                      m)
-           profs-per-student))
+  (is (= (p/transform {prof {_ [{:keys [name]}]}}
+                      {name #{prof}}
+                      profs->classes->students)
+         students->profs))
 
-    (is (= (p/collect {prof {class [{:let {student :name
-                                           grade :grade}
-                                     :where (= grade "A")}]}}
-                      ;; all students who have an A in some class
-                      #{student}
-                      m)
-           #{"John" "Ben Bitdiddle"}))
+  (is (= (p/transform {prof {_ [{name :name, grade :grade}]}}
+                      {name #{prof}}
+                      profs->classes->students)
+         students->profs))
 
-    ;; Map from students to set of classes they have an A in
-    (is (= (p/collect {prof {class [{:let {student :name
-                                           grade :grade}}]}}
-                      {student (if (= grade "A")
-                                 #{class}
-                                 #{})}
-                      m)
-           {"John" #{"AI"}
-            "Ben Bitdiddle" #{"Compilers"}
-            "Eva Lu Ator" #{}
-            "Sally" #{}
-            "Tom" #{}}))
+  (is (thrown? Exception (p/compile '{prof {_ [{name :name}]}}
+                                    '{name #{prof}}))))
 
-    (is (= (p/collect {prof {:extract (p/key= "Compilers")
-                             class [{:let {student :name
-                                           grade :grade}
-                                     :where (= grade "A")}]}}
-                      ;; all students who have an A in Compilers
-                      #{student}
-                      m)
-           #{"Ben Bitdiddle"}))
 
-    ;; swap the order of professor and class
-    (let [class->prof {"AI"
-                       {"Sussman"
-                        [{:name "John"
-                          :grade "A"}
-                         {:name "Sally"
-                          :grade "B"}]}
-                       "Compilers"
-                       {"Sussman"
-                        [{:name "Tom"
-                          :grade "B"}
-                         {:name "John"
-                          :grade "B"}]
-                        "Abelson"
-                        [{:name "Eva Lu Ator"
-                          :grade "B"}
-                         {:name "Ben Bitdiddle"
-                          :grade "A"}]}
-                       "Machine Learning"
-                       {"Abelson"
-                        [{:name "Sally"
-                          :grade "C"}
-                         {:name "Tom"
-                          :grade "B-"}]}}
-          swap-key-order (p/collector {k1 {k2 v}} {k2 {k1 v}})]
-      (is (= (swap-key-order m) class->prof))
-      (is (= (swap-key-order (swap-key-order m)) m)))))
-
-(deftest cumbersome-collections
-  (let [m [{:class "Compilers"
-            :professor "Sussman"
-            :room "West 707"
-            :students ["Eva Lu Ator" "Ben Bitdiddle"]}
-           {:class "AI"
-            :professor "Abelson"
-            :room "East 103"
-            :students ["Eva Lu Ator" "Sally"]}]]
-    ;; student -> rooms they have class in
-    (is (= (p/collect [{:let {room :room}
-                        :extract (p/key= :students)
-                        _ [student]}]
-                      {student #{room}}
-                      m)
-           {"Eva Lu Ator" #{"West 707" "East 103"}
-            "Ben Bitdiddle" #{"West 707"}
-            "Sally" #{"East 103"}}))))
-
-(deftest vectors->maps
-  (let [pieces [{:composer "Bartok"
-                 :title "Piano Concerto 1"
-                 :year 1926}
-                {:composer "Bartok"
-                 :title "String Quartet 2"
-                 :year 1917}
-                {:composer "Ligeti"
-                 :title "Etude 1"
-                 :year 1985}]]
-    (is (= (p/collect [{:let {composer :composer
-                              :rest rest}}]
-                      {composer #{rest}}
-                      pieces)
-           {"Bartok"
-            #{{:title "Piano Concerto 1"
-               :year 1926}
-              {:title "String Quartet 2"
-               :year 1917}}
-            "Ligeti"
-            #{{:title "Etude 1"
-               :year 1985}}}))))
+;;    ;; Map from students to set of classes they have an A in
+;;    (is (= (p/collect {prof {class [{:let {student :name
+;;                                           grade :grade}}]}}
+;;                      {student (if (= grade "A")
+;;                                 #{class}
+;;                                 #{})}
+;;                      m)
+;;           {"John" #{"AI"}
+;;            "Ben Bitdiddle" #{"Compilers"}
+;;            "Eva Lu Ator" #{}
+;;            "Sally" #{}
+;;            "Tom" #{}}))
+;;
+;;    (is (= (p/collect {prof {:extract (p/key= "Compilers")
+;;                             class [{:let {student :name
+;;                                           grade :grade}
+;;                                     :where (= grade "A")}]}}
+;;                      ;; all students who have an A in Compilers
+;;                      #{student}
+;;                      m)
+;;           #{"Ben Bitdiddle"}))
+;;
+;;    ;; swap the order of professor and class
+;;    (let [class->prof {"AI"
+;;                       {"Sussman"
+;;                        [{:name "John"
+;;                          :grade "A"}
+;;                         {:name "Sally"
+;;                          :grade "B"}]}
+;;                       "Compilers"
+;;                       {"Sussman"
+;;                        [{:name "Tom"
+;;                          :grade "B"}
+;;                         {:name "John"
+;;                          :grade "B"}]
+;;                        "Abelson"
+;;                        [{:name "Eva Lu Ator"
+;;                          :grade "B"}
+;;                         {:name "Ben Bitdiddle"
+;;                          :grade "A"}]}
+;;                       "Machine Learning"
+;;                       {"Abelson"
+;;                        [{:name "Sally"
+;;                          :grade "C"}
+;;                         {:name "Tom"
+;;                          :grade "B-"}]}}
+;;          swap-key-order (p/collector {k1 {k2 v}} {k2 {k1 v}})]
+;;      (is (= (swap-key-order m) class->prof))
+;;      (is (= (swap-key-order (swap-key-order m)) m)))))
+;;
+;;(deftest cumbersome-collections
+;;  (let [m [{:class "Compilers"
+;;            :professor "Sussman"
+;;            :room "West 707"
+;;            :students ["Eva Lu Ator" "Ben Bitdiddle"]}
+;;           {:class "AI"
+;;            :professor "Abelson"
+;;            :room "East 103"
+;;            :students ["Eva Lu Ator" "Sally"]}]]
+;;    ;; student -> rooms they have class in
+;;    (is (= (p/collect [{:let {room :room}
+;;                        :extract (p/key= :students)
+;;                        _ [student]}]
+;;                      {student #{room}}
+;;                      m)
+;;           {"Eva Lu Ator" #{"West 707" "East 103"}
+;;            "Ben Bitdiddle" #{"West 707"}
+;;            "Sally" #{"East 103"}}))))
+;;
+;;(deftest vectors->maps
+;;  (let [pieces [{:composer "Bartok"
+;;                 :title "Piano Concerto 1"
+;;                 :year 1926}
+;;                {:composer "Bartok"
+;;                 :title "String Quartet 2"
+;;                 :year 1917}
+;;                {:composer "Ligeti"
+;;                 :title "Etude 1"
+;;                 :year 1985}]]
+;;    (is (= (p/collect [{:let {composer :composer
+;;                              :rest rest}}]
+;;                      {composer #{rest}}
+;;                      pieces)
+;;           {"Bartok"
+;;            #{{:title "Piano Concerto 1"
+;;               :year 1926}
+;;              {:title "String Quartet 2"
+;;               :year 1917}}
+;;            "Ligeti"
+;;            #{{:title "Etude 1"
+;;               :year 1985}}}))))
